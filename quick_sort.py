eae026eb7c9d41d6101e73bc808ab895e8b8198e
Ubuntu <ravi@azure-vm.5i1wxvrh1trutifwtweocfwbvf.rx.internal.cloudapp.net>
2025-06-09 12:20:06 +0000

    update quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -4,10 +4,12 @@ def quicksort(arr):
     
     mid = len(arr)//2
     pivot= arr[mid]
+    middle = [i for i in arr if i==pivot]
     left_side = [i for i in arr if i<pivot]
     right_side = [i for i in arr if i>pivot]
-    return quicksort(left_side)+[pivot]+quicksort(right_side)
+    return quicksort(left_side)+middle+quicksort(right_side)
 
+print(quicksort([3,2,2,2,2,2,2,2,2,1]))
 
 print(quicksort([6235,235,347,1252,75,412,25,4,457,5412,4543,6,456,4235,6,43436,54,712,4,437]))
 print(quicksort([6235,235,347,1252,75,412,25,4,457,5412,4543,6,456,4235,6,43436,54,712,4]))
